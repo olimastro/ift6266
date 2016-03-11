00dce0933c7c18a5d471322b5f4c311add5545ab
Olivier Mastropietro <oli.mastro@gmail.com>
2016-03-11 18:16:46 -0500

    Implementation of real first model with lstm brick

--- rnn_em.py
+++ rnn_em.py
@@ -4,16 +4,14 @@ import theano.tensor as T
 import sys
 import matplotlib.pylab as plt
 
-from blocks.bricks import MLP, Tanh
-from blocks.algorithms import GradientDescent, Scale
+from blocks import initialization
+from blocks.bricks import Tanh
+from blocks.bricks.recurrent import LSTM
 from blocks.graph import ComputationGraph
 from blocks.initialization import IsotropicGaussian, Constant
-from blocks.extensions import FinishAfter, Timing
-from blocks.main_loop import MainLoop
-from blocks.model import Model
 
-from fuel.streams import DataStream
-from fuel.schemes import SequentialScheme
+from scipy.io.wavfile import read
+from scipy.io.wavfile import write
 
 from sklearn.mixture import GMM
 
@@ -21,37 +19,42 @@ np.random.seed(1234)
 np.seterr(all='warn')
 
 """
-    This class will train a mixture of gaussian
-    on top of an RNN in a back and forth fashion.
+    This class will train a mixture of gaussian.
+    First, an RNN will produce a hidden state.
+    Second, the EM algorithm will fit a GMM over the next time step
+    which is the target.
+    Third, the gradient will be computed with the LL of this GMM
+    and the hidden state.
 """
 class RNN_EM :
-    def __init__(self, number_of_mix=3, learning_rate=0.01, input_dim=4800):
+    def __init__(self, number_of_mix=3, learning_rate=0.01, input_dim=4800, samplerate=48000):
         self.lr = learning_rate
-        self.bprop, self.fprop = self.build_theano_functions()
         self.number_of_mix = number_of_mix
         self.input_dim = input_dim
+        self.samplerate = samplerate
 
-        self.extensions = [Timing(), FinishAfter(after_n_epochs=1)]
-
+        self.bprop, self.fprop = self.build_theano_functions()
 
     # Theano will take care of everything before the EM step.
     # We have to give it the fixed param of the EM for computation of LL.
     def build_theano_functions(self) :
-        x = T.dvector('x')
-        y = T.dvector('y')
-        s = T.dvector('s')
-        mu = T.dvector('mu')
-        mu = T.reshape(mu,(3,1))
-        pi = T.dvector('pi')
+        #import pdb ; pdb.set_trace()
+        x = T.fmatrix('x')
+        s = T.fvector('s')
+        mu = T.fvector('mu')
+        mu = T.reshape(mu,(self.number_of_mix,1))
+        pi = T.fvector('pi')
 
-        mlp = MLP([Tanh()], [4800,4800],
-                  weights_init=IsotropicGaussian(0.01),
-                  biases_init=Constant(0))
-        mlp.initialize()
-        h = mlp.apply(x)
+        lstm = LSTM(
+            dim=self.input_dim/4,
+            weights_init=IsotropicGaussian(0.5),
+            biases_init=Constant(1))
+        lstm.initialize()
+        h, c = lstm.apply(x)
+        h = h[0][0][-1]
 
-        LL = T.sum(pi*(1./(T.sqrt(2.*np.pi)*s))*(\
-            -0.5*(y-mu)**2/T.reshape(s,(3,1))**2.).sum(axis=1))
+        LL = T.sum(pi*(1./(T.sqrt(2.*np.pi)*s))*T.exp(\
+            -0.5*(h-mu)**2/T.reshape(s,(self.number_of_mix,1))**2.).sum(axis=1))
         cost = -T.log(LL)
 
         cg = ComputationGraph(cost)
@@ -61,33 +64,23 @@ class RNN_EM :
         for i in range(len(grads)) :
             updates.append(tuple([parameters[i], parameters[i] - self.lr*grads[i]]))
 
-        gradf = theano.function([x,y,s,mu,pi],[h,LL],updates=updates)
+        gradf = theano.function([x,s,mu,pi],[cost],updates=updates)
         f = theano.function([x],[h])
 
         return gradf, f
 
 
     def init_em_model(self, data):
-        inc = 48000
-
-        sigmas = np.zeros((len(data)/inc, self.number_of_mix))
-        mus = np.zeros((len(data)/inc, self.number_of_mix))
-        pis = np.zeros((len(data)/inc, self.number_of_mix))
+        data = data.reshape((len(data),1))
+        inc = self.samplerate
 
         gmm = GMM(self.number_of_mix, covariance_type="spherical")
         gmm.fit(data[0:inc])
-
-        mus[0] = gmm.means_.reshape(self.number_of_mix,)
-        sigmas[0] = gmm.covars_.reshape(self.number_of_mix,)
-        pis[0] = gmm.weights_.reshape(self.number_of_mix,)
         gmm.init_params = ""
 
-        for i in range(inc, len(data)-inc, inc):
+        #for i in range(inc, len(data)-inc, inc):
+        for i in range(inc, len(data)/1000, inc):
             gmm.fit(data[i:i+inc])
-
-            mus[i/inc] = gmm.means_.reshape(self.number_of_mix,)
-            sigmas[i/inc] = gmm.covars_.reshape(self.number_of_mix,)
-            pis[i/inc] = gmm.weights_.reshape(self.number_of_mix,)
 
         self.gmm = gmm
 
@@ -96,25 +89,54 @@ class RNN_EM :
         mus = self.gmm.means_.reshape(self.number_of_mix,)
         sigmas = self.gmm.covars_.reshape(self.number_of_mix,)
         pis = self.gmm.weights_.reshape(self.number_of_mix,)
-        return [sigmas, mus, pis]
+        return np.array([sigmas, mus, pis], dtype=np.float32)
 
 
-    def train(self, data, epochs=1):
-        np.random.shuffle(data)
+    def train(self, data, epochs=50):
+        data = self.prepare_data(data)
+        print "Initializing GMM"
         self.init_em_model(data)
 
+        blocks = float(np.floor(self.samplerate*60.*5.))
+        costs = np.zeros((epochs,len(data)/blocks))
+
         for epoch in range(epochs):
-            for i in (0, len(data)-self.input_dim, self.input_dim) :
+            print
+            print "New epoch #", epoch
+            cost = 0.
+            j = 0
+            k = 1
+            for i in range(0, len(data)-self.input_dim, self.input_dim) :
+                j+=1
+                sys.stdout.write('\rComputing LL on %d/%d examples'%(i, data.shape[0]))
+                sys.stdout.flush()
                 x = data[i:i+self.input_dim].flatten()
-                h = self.fprop(x)
-                # The RNN should have produced the next distribution over x_t
-                self.gmm.fit(np.array(h).reshape((self.input_dim,1)))
+                y = data[i+self.input_dim:i+2*self.input_dim].flatten()
+                y = y.reshape((len(y),1))
+                self.gmm.fit(y)
                 gmm_param_list = self.get_gmm_param()
                 # Train the RNN with the likelihood over the parametrized distribution (found with EM)
                 # with x_t+1
-                y = data[i+self.input_dim:i+2*self.input_dim].flatten()
-                self.bprop(x,y,gmm_param_list[0],gmm_param_list[1].reshape((3,1)),gmm_param_list[2])
+                _cost = self.bprop(x[np.newaxis],gmm_param_list[0],gmm_param_list[1].reshape((self.number_of_mix,1)),gmm_param_list[2])
+                cost += _cost[0]
 
+                nblocks = np.floor(i/blocks)
+                if nblocks >= k and nblocks <= costs.shape[1] :
+                    k+=1
+                    costs[epoch,nblocks-1] = cost-np.sum(costs[epoch,:nblocks-1])
+
+            f = open("/Tmp/mastropo/rnn_em_cost_gpu.npy",'w')
+            np.save(f, costs)
+            f.close()
+
+
+    # normalize the data in [-1,1]
+    # and bring it to 0 mean 1 variance
+    def prepare_data(self, data) :
+        data = data.astype(np.float32)
+        data = data/np.max(data)
+        data = (data-np.average(data))/np.std(data)
+        return data
 
 
 
@@ -135,5 +157,10 @@ def make_data(amount=1000000):
 
 
 if __name__ == "__main__" :
-    model = RNN_EM()
-    model.train(make_data())
+    print "Loading data"
+    data = read("/Tmp/mastropo/XqaJ2Ol5cC4.wav")
+    samplerate = data[0]
+    data = data[1]
+
+    model = RNN_EM(input_dim=2*(samplerate/10), samplerate=samplerate)
+    model.train(data)
